e111d410622d84b6966a5549602b509bf1f1447f
salarsalarsalar <salarahmed9876@gmail.com>
2025-04-15 16:49:23 +0500

    Added SSL

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const iotRoutes = require('./routes/iotRoutes');
 const userRoutes = require('./routes/userRoutes'); 
 const app = express();
 const errorHandler = require('./middleware/error')
+const fs = require('fs');
+const https = require('https');
 
 dotenv.config(); // Load .env variables
 
@@ -20,9 +22,14 @@ app.use('/api/user', userRoutes);
 // Error Handling
 app.use(errorHandler);
 
+// SSL setup
+const sslOptions = {
+  key: fs.readFileSync('./cert/server.key'),
+  cert: fs.readFileSync('./cert/server.cert')
+};
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`Secure server running on https://localhost:${PORT}`);
 });
 
